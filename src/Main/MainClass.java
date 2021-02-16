2cc94ab58ed00e46e4415fe55eb756d6e4d65239
seunghyun <dhdpdlfjs@naver.com>
2021-02-16 15:35:39 +0900

    메인 기능 추가

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -1,5 +1,21 @@
 package Main;
 
-public class MainClass {
+import java.util.Scanner;
 
+public class MainClass {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	int a=0, b=0, num=0;
+	
+	
+	while(true) {
+		System.out.println("========숫자 프로그램==========\n 1.최소공배수\n 2.최대공약수 \n 3.두 수의 합");
+		num = input.nextInt();
+	switch(num){
+		case 1:
+		case 2:
+		case 3:
+	}
+	}
+}
 }
